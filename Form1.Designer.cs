916ddaa4c317f6f76e453865f931cf93d2c88dc1
Peter Hutchison <pjhutch17@talktalk.net>
2022-01-03 09:49:37 +0000

    Upload project files

---
+++ Form1.Designer.cs
@@ -0,0 +1,321 @@
+﻿namespace PoSeiden_2019
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblClickResult = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ClrEntry = new System.Windows.Forms.Button();
+            this.cmd_ClearAll = new System.Windows.Forms.Button();
+            this.cmd_Num1 = new System.Windows.Forms.Button();
+            this.cmd_Num2 = new System.Windows.Forms.Button();
+            this.cmd_Num3 = new System.Windows.Forms.Button();
+            this.cmd_Num4 = new System.Windows.Forms.Button();
+            this.cmd_Num5 = new System.Windows.Forms.Button();
+            this.cmd_Num6 = new System.Windows.Forms.Button();
+            this.cmd_Num7 = new System.Windows.Forms.Button();
+            this.cmd_Num8 = new System.Windows.Forms.Button();
+            this.cmd_NumDot = new System.Windows.Forms.Button();
+            this.cmd_Num9 = new System.Windows.Forms.Button();
+            this.cmd_Num0 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_CashTend = new System.Windows.Forms.TextBox();
+            this.Label3 = new System.Windows.Forms.Label();
+            this.lbl_Change = new System.Windows.Forms.Label();
+            this.cmd_Enter = new System.Windows.Forms.Button();
+            this.cmd_ClrNumEntry = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblClickResult
+            // 
+            this.lblClickResult.Location = new System.Drawing.Point(169, 609);
+            this.lblClickResult.Name = "lblClickResult";
+            this.lblClickResult.Size = new System.Drawing.Size(67, 20);
+            this.lblClickResult.TabIndex = 0;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(39, 430);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(124, 173);
+            this.listBox1.TabIndex = 1;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(169, 430);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(67, 173);
+            this.listBox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 610);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Subtotal:";
+            // 
+            // ClrEntry
+            // 
+            this.ClrEntry.Location = new System.Drawing.Point(255, 430);
+            this.ClrEntry.Name = "ClrEntry";
+            this.ClrEntry.Size = new System.Drawing.Size(98, 26);
+            this.ClrEntry.TabIndex = 4;
+            this.ClrEntry.Text = "Clear Entry";
+            this.ClrEntry.UseVisualStyleBackColor = true;
+            this.ClrEntry.Click += new System.EventHandler(this.cmd_ClrEntry_Click);
+            // 
+            // cmd_ClearAll
+            // 
+            this.cmd_ClearAll.Location = new System.Drawing.Point(255, 479);
+            this.cmd_ClearAll.Name = "cmd_ClearAll";
+            this.cmd_ClearAll.Size = new System.Drawing.Size(75, 42);
+            this.cmd_ClearAll.TabIndex = 5;
+            this.cmd_ClearAll.Text = "Clear All";
+            this.cmd_ClearAll.UseVisualStyleBackColor = true;
+            this.cmd_ClearAll.Click += new System.EventHandler(this.cmd_ClearAll_Click);
+            // 
+            // cmd_Num1
+            // 
+            this.cmd_Num1.Location = new System.Drawing.Point(631, 430);
+            this.cmd_Num1.Name = "cmd_Num1";
+            this.cmd_Num1.Size = new System.Drawing.Size(48, 40);
+            this.cmd_Num1.TabIndex = 6;
+            this.cmd_Num1.Text = "1";
+            this.cmd_Num1.UseVisualStyleBackColor = true;
+            // 
+            // cmd_Num2
+            // 
+            this.cmd_Num2.Location = new System.Drawing.Point(685, 430);
+            this.cmd_Num2.Name = "cmd_Num2";
+            this.cmd_Num2.Size = new System.Drawing.Size(48, 40);
+            this.cmd_Num2.TabIndex = 7;
+            this.cmd_Num2.Text = "2";
+            this.cmd_Num2.UseVisualStyleBackColor = true;
+            // 
+            // cmd_Num3
+            // 
+            this.cmd_Num3.Location = new System.Drawing.Point(743, 430);
+            this.cmd_Num3.Name = "cmd_Num3";
+            this.cmd_Num3.Size = new System.Drawing.Size(48, 40);
+            this.cmd_Num3.TabIndex = 8;
+            this.cmd_Num3.Text = "3";
+            this.cmd_Num3.UseVisualStyleBackColor = true;
+            // 
+            // cmd_Num4
+            // 
+            this.cmd_Num4.Location = new System.Drawing.Point(801, 430);
+            this.cmd_Num4.Name = "cmd_Num4";
+            this.cmd_Num4.Size = new System.Drawing.Size(48, 40);
+            this.cmd_Num4.TabIndex = 9;
+            this.cmd_Num4.Text = "4";
+            this.cmd_Num4.UseVisualStyleBackColor = true;
+            // 
+            // cmd_Num5
+            // 
+            this.cmd_Num5.Location = new System.Drawing.Point(631, 476);
+            this.cmd_Num5.Name = "cmd_Num5";
+            this.cmd_Num5.Size = new System.Drawing.Size(48, 40);
+            this.cmd_Num5.TabIndex = 10;
+            this.cmd_Num5.Text = "5";
+            this.cmd_Num5.UseVisualStyleBackColor = true;
+            // 
+            // cmd_Num6
+            // 
+            this.cmd_Num6.Location = new System.Drawing.Point(685, 476);
+            this.cmd_Num6.Name = "cmd_Num6";
+            this.cmd_Num6.Size = new System.Drawing.Size(48, 40);
+            this.cmd_Num6.TabIndex = 11;
+            this.cmd_Num6.Text = "6";
+            this.cmd_Num6.UseVisualStyleBackColor = true;
+            // 
+            // cmd_Num7
+            // 
+            this.cmd_Num7.Location = new System.Drawing.Point(743, 476);
+            this.cmd_Num7.Name = "cmd_Num7";
+            this.cmd_Num7.Size = new System.Drawing.Size(48, 40);
+            this.cmd_Num7.TabIndex = 12;
+            this.cmd_Num7.Text = "7";
+            this.cmd_Num7.UseVisualStyleBackColor = true;
+            // 
+            // cmd_Num8
+            // 
+            this.cmd_Num8.Location = new System.Drawing.Point(801, 476);
+            this.cmd_Num8.Name = "cmd_Num8";
+            this.cmd_Num8.Size = new System.Drawing.Size(48, 40);
+            this.cmd_Num8.TabIndex = 13;
+            this.cmd_Num8.Text = "8";
+            this.cmd_Num8.UseVisualStyleBackColor = true;
+            // 
+            // cmd_NumDot
+            // 
+            this.cmd_NumDot.Location = new System.Drawing.Point(743, 523);
+            this.cmd_NumDot.Name = "cmd_NumDot";
+            this.cmd_NumDot.Size = new System.Drawing.Size(48, 40);
+            this.cmd_NumDot.TabIndex = 14;
+            this.cmd_NumDot.Text = ".";
+            this.cmd_NumDot.UseVisualStyleBackColor = true;
+            // 
+            // cmd_Num9
+            // 
+            this.cmd_Num9.Location = new System.Drawing.Point(631, 523);
+            this.cmd_Num9.Name = "cmd_Num9";
+            this.cmd_Num9.Size = new System.Drawing.Size(48, 40);
+            this.cmd_Num9.TabIndex = 15;
+            this.cmd_Num9.Text = "9";
+            this.cmd_Num9.UseVisualStyleBackColor = true;
+            // 
+            // cmd_Num0
+            // 
+            this.cmd_Num0.Location = new System.Drawing.Point(685, 523);
+            this.cmd_Num0.Name = "cmd_Num0";
+            this.cmd_Num0.Size = new System.Drawing.Size(48, 40);
+            this.cmd_Num0.TabIndex = 16;
+            this.cmd_Num0.Text = "0";
+            this.cmd_Num0.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(626, 577);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Cash Tend:";
+            // 
+            // txt_CashTend
+            // 
+            this.txt_CashTend.Location = new System.Drawing.Point(769, 571);
+            this.txt_CashTend.Name = "txt_CashTend";
+            this.txt_CashTend.Size = new System.Drawing.Size(123, 20);
+            this.txt_CashTend.TabIndex = 18;
+            // 
+            // Label3
+            // 
+            this.Label3.AutoSize = true;
+            this.Label3.Location = new System.Drawing.Point(632, 617);
+            this.Label3.Name = "Label3";
+            this.Label3.Size = new System.Drawing.Size(47, 13);
+            this.Label3.TabIndex = 19;
+            this.Label3.Text = "Change:";
+            // 
+            // lbl_Change
+            // 
+            this.lbl_Change.AutoSize = true;
+            this.lbl_Change.Location = new System.Drawing.Point(769, 621);
+            this.lbl_Change.Name = "lbl_Change";
+            this.lbl_Change.Size = new System.Drawing.Size(124, 13);
+            this.lbl_Change.TabIndex = 20;
+            this.lbl_Change.Text = "                                       ";
+            // 
+            // cmd_Enter
+            // 
+            this.cmd_Enter.Location = new System.Drawing.Point(801, 523);
+            this.cmd_Enter.Name = "cmd_Enter";
+            this.cmd_Enter.Size = new System.Drawing.Size(48, 40);
+            this.cmd_Enter.TabIndex = 21;
+            this.cmd_Enter.Text = "Enter";
+            this.cmd_Enter.UseVisualStyleBackColor = true;
+            // 
+            // cmd_ClrNumEntry
+            // 
+            this.cmd_ClrNumEntry.Location = new System.Drawing.Point(855, 430);
+            this.cmd_ClrNumEntry.Name = "cmd_ClrNumEntry";
+            this.cmd_ClrNumEntry.Size = new System.Drawing.Size(48, 40);
+            this.cmd_ClrNumEntry.TabIndex = 22;
+            this.cmd_ClrNumEntry.Text = "CE";
+            this.cmd_ClrNumEntry.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1021, 659);
+            this.Controls.Add(this.cmd_ClrNumEntry);
+            this.Controls.Add(this.cmd_Enter);
+            this.Controls.Add(this.lbl_Change);
+            this.Controls.Add(this.Label3);
+            this.Controls.Add(this.txt_CashTend);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cmd_Num0);
+            this.Controls.Add(this.cmd_Num9);
+            this.Controls.Add(this.cmd_NumDot);
+            this.Controls.Add(this.cmd_Num8);
+            this.Controls.Add(this.cmd_Num7);
+            this.Controls.Add(this.cmd_Num6);
+            this.Controls.Add(this.cmd_Num5);
+            this.Controls.Add(this.cmd_Num4);
+            this.Controls.Add(this.cmd_Num3);
+            this.Controls.Add(this.cmd_Num2);
+            this.Controls.Add(this.cmd_Num1);
+            this.Controls.Add(this.cmd_ClearAll);
+            this.Controls.Add(this.ClrEntry);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.lblClickResult);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox lblClickResult;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button ClrEntry;
+        private System.Windows.Forms.Button cmd_ClearAll;
+        private System.Windows.Forms.Button cmd_Num1;
+        private System.Windows.Forms.Button cmd_Num2;
+        private System.Windows.Forms.Button cmd_Num3;
+        private System.Windows.Forms.Button cmd_Num4;
+        private System.Windows.Forms.Button cmd_Num5;
+        private System.Windows.Forms.Button cmd_Num6;
+        private System.Windows.Forms.Button cmd_Num7;
+        private System.Windows.Forms.Button cmd_Num8;
+        private System.Windows.Forms.Button cmd_NumDot;
+        private System.Windows.Forms.Button cmd_Num9;
+        private System.Windows.Forms.Button cmd_Num0;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_CashTend;
+        private System.Windows.Forms.Label Label3;
+        private System.Windows.Forms.Label lbl_Change;
+        private System.Windows.Forms.Button cmd_Enter;
+        private System.Windows.Forms.Button cmd_ClrNumEntry;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,219 @@
+﻿// Program: PoSeiden
+// Purpose: Point of Sale program
+// By P J Hutchison
+// 21 Jul 2019
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace PoSeiden_2019
+{
+    public partial class Form1 : Form
+    {
+        Button[] POS_btn;
+        string[] POS_Text, POS_Name,  POS_Code;
+        double[] POS_Price;
+        double SaleTotal = 0;
+        string CurrencySymbol = "£";
+
+
+        public Form1()
+        {
+            Int32 btn_count;
+            string ItemCode = "", ItemName = "", ItemLine;
+            string[] ItemList;
+            double ItemPrice = 0.0F;
+            StreamReader POS_File;
+            InitializeComponent();
+
+            // Init arrays
+            POS_btn = new Button[41];
+            POS_Text = new string[41];
+            POS_Name = new string[41];
+            POS_Code = new string[41];
+            POS_Price = new double[41];
+
+            // Read in POS information from a file
+            Console.WriteLine("Directory = " + Directory.GetCurrentDirectory());
+            if (File.Exists("..\\..\\POS_Information.txt"))
+            {
+                //FileOpen(1, "..\..\POS_Information.txt", OpenMode.Input, OpenAccess.Read, OpenShare.LockRead)
+                POS_File = new StreamReader("..\\..\\POS_Information.txt");
+                btn_count = 1;
+                while (!POS_File.EndOfStream)
+                {
+                    //Input ItemCode, ItemName, ItemPrice from file.
+                    ItemLine = POS_File.ReadLine();
+                    ItemList = ItemLine.Split(',');
+
+                    ItemCode = (ItemList[0] + "").Trim();
+                    ItemName = ItemList[1] + "";
+                    ItemPrice = Convert.ToDouble(ItemList[2]+"");
+                    POS_Text[btn_count] = ItemCode + Convert.ToChar(13) + ItemName + Convert.ToChar(13) + ItemPrice.ToString("c");
+                    Console.WriteLine(POS_Text[btn_count]);
+                    POS_Name[btn_count] = ItemName;
+                    POS_Price[btn_count] = ItemPrice;
+                    POS_Code[btn_count] = ItemCode;
+                    btn_count = btn_count + 1;
+                }
+                POS_File.Close();
+            }
+            else
+            {
+                MessageBox.Show("POS Information file not found!");
+                return;
+
+            }
+
+            // Draw buttons on the form
+            Add_Buttons_to_Form();
+
+            // Add handler for Num Pad buttons
+            cmd_Num1.Click += new System.EventHandler(this.NumPad_Click);
+            cmd_Num2.Click += new System.EventHandler(this.NumPad_Click);
+            cmd_Num3.Click += new System.EventHandler(this.NumPad_Click);
+            cmd_Num4.Click += new System.EventHandler(this.NumPad_Click);
+            cmd_Num5.Click += new System.EventHandler(this.NumPad_Click);
+            cmd_Num6.Click += new System.EventHandler(this.NumPad_Click);
+            cmd_Num7.Click += new System.EventHandler(this.NumPad_Click);
+            cmd_Num8.Click += new System.EventHandler(this.NumPad_Click);
+            cmd_Num9.Click += new System.EventHandler(this.NumPad_Click);
+            cmd_Num0.Click += new System.EventHandler(this.NumPad_Click);
+            cmd_NumDot.Click += new System.EventHandler(this.NumPad_Click);
+            cmd_Enter.Click += new System.EventHandler(this.NumPad_Click);
+            cmd_ClrNumEntry.Click += new System.EventHandler(this.NumPad_Click);
+        }
+
+
+        string TrimNum(string value)
+        {
+            string Result;
+            Int16 n;
+
+            Result = "";
+            for (n=0; n < (value.Length); n++)
+            {
+                if (value[n] >= '0' & value[n] <= '9')
+                    Result = Result + value[n];
+            }
+            return (Result);
+        }
+        void Add_Buttons_to_Form()
+        {
+            Int32 x, y, btn_count;
+
+            btn_count = 1;
+            for (y=1; y<=4; y++)
+            {
+                for (x=1; x<=10; x++)
+                {
+                    // Add a button
+                    POS_btn[btn_count] = new Button();
+                    POS_btn[btn_count].Left = 7 + (x - 1) * 100;
+                    POS_btn[btn_count].Top = 10 + (y - 1) * 100;
+                    POS_btn[btn_count].Width = 95;
+                    POS_btn[btn_count].Height = 95;
+                    POS_btn[btn_count].Text = POS_Text[btn_count];
+                    POS_btn[btn_count].ForeColor = Color.Black;
+                    POS_btn[btn_count].Tag = btn_count; // Stores ID of button
+                    // Add an event handler
+                    POS_btn[btn_count].Click += new System.EventHandler(btn_Click);
+                    // Add button to the form
+                    this.Controls.Add(POS_btn[btn_count]);
+                    btn_count = btn_count + 1;
+                    if (btn_count > POS_btn.GetUpperBound(0))
+                        break; // exit for x loop
+                }
+                if (btn_count > POS_btn.GetUpperBound(0))
+                    break; // exit for y loop
+            }
+        }
+
+        void btn_Click (object sender, EventArgs e)
+        {
+            Button button = new Button();
+            Int32 idx = 0;
+
+            // Convert object to a button type so I can access attributes
+            button = (Button)sender;
+
+            idx = Convert.ToInt32(button.Tag);
+            Console.WriteLine("A button was pressed. Button #" + idx.ToString());
+            SaleTotal = SaleTotal + Convert.ToDouble(POS_Price[idx]);
+            // Converts numbers to £##.## values
+            lblClickResult.Text = SaleTotal.ToString("##.##");
+            listBox1.Items.Add(POS_Name[idx]);
+            listBox2.Items.Add(POS_Price[idx].ToString("##.##"));
+        }
+
+        void cmd_ClearAll_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            SaleTotal = 0;
+            lblClickResult.Text = SaleTotal.ToString();
+        }
+
+        void cmd_ClrEntry_Click(object sender, EventArgs e)
+        {
+            Int32 item;
+            string itemName;
+            double itemPrice;
+
+            item = listBox1.SelectedIndex;
+            if (item == -1)  return;
+            itemName = listBox1.Items[item].ToString();
+            itemPrice = Convert.ToDouble(listBox2.Items[item]);
+            itemPrice = -itemPrice;
+            // Add item to list but as a removal
+            listBox1.Items.Add(itemName);
+            listBox2.Items.Add(itemPrice);
+            // subtract item price
+            SaleTotal = SaleTotal + itemPrice;
+            lblClickResult.Text = SaleTotal.ToString();
+        }
+
+        void NumPad_Click(object sender, EventArgs e)
+        {
+            Button NumButton;
+            string NumValue;
+            double CashTend, Change;
+
+            // Get button pressed
+            NumButton = (Button)sender;
+            // Get value of button from Text field
+            NumValue = NumButton.Text;
+            // If a digit or dot then add it to the CashTend field
+            if (NumValue[0] >= '0' & NumValue[0] <= '9')
+                txt_CashTend.Text = txt_CashTend.Text + NumValue;
+            else if (NumValue[0] == '.' & (txt_CashTend.Text).IndexOf (".") == -1 )
+                txt_CashTend.Text = txt_CashTend.Text + NumValue;
+            else if (NumValue == "Enter")
+            {
+                // Work out the change from cash sale
+                CashTend = Convert.ToDouble (txt_CashTend.Text);
+                Change = CashTend - SaleTotal;
+                lbl_Change.Text = CurrencySymbol + Change.ToString();
+            }
+            else if (NumValue == "CE")
+            {
+                CashTend = 0;
+                Change = 0;
+                txt_CashTend.Text = "";
+                lbl_Change.Text = "";
+            }
+        }
+    }
+
+    
+}
